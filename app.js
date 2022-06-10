39f79d391a9721a0ca932c9fb06d2811896c321d
AstralRider <100898882+AstralRider@users.noreply.github.com>
2022-06-10 21:04:26 +1000

    successfully got the get request to return data

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const multiplyByTwo = num => {
-    return num * 2
-}
+
+const getData = async () => {
+    const options = { 
+        method: 'GET',
+        headers: {
+          "Accept" : "application/json" 
+        }
+      };
+
+    try {
+        const response = await fetch('https://icanhazdadjoke.com/', options)
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse)
+        }
+        throw new Error('Request failed!')
+
+    } catch(error) {
+        console.log(error)
+    }
+
+};
+
+getData()
